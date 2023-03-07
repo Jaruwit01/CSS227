ee3c1a4ff1ec5eeb51e4bcf0ffac25287c0a4cd6
jaruwit01 <jaruwit178@gmail.com>
2023-03-07 15:05:06 +0700

    update

--- Learning/JS/Learn1.js
+++ Learning/JS/Learn1.js
@@ -1,3 +1,11 @@
-const prompt = require('prompt-sync')();
-let old = prompt('What old are you? : ');
-console.log('You are ' + old + ' years old');
+const iTime = document.querySelector('.time');
+
+function setTime(){
+    const time = new Date();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    iTime.innerHTML = `${hours<10 ?`0${houser}`:hours}:${minutes<10 ?`0${minutes}`:minutes}:${seconds<10 ?`0${seconds}`:seconds}`;
+} 
+setTime();
+setInterval(setTime, 1000);
